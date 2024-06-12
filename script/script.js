6519569aa68651b9a591e61769badd7e8f272e6c
Fabiano Júnior Lima Barbosa <138934847+Fabito02@users.noreply.github.com>
2024-06-12 18:30:46 -0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,31 +1,58 @@
 //bgl pra animar o botão quando eu clicar
-function animacaoDoMenu() {
-    let menu = document.getElementById('menu')
-    menu.classList.toggle('active')
-}
-function mostrarRedesSociais() {
-    var list = document.getElementById("social");
-    var drop = document.getElementById("dropdown");
-    var seta = document.getElementById("setaAnimada");
-    seta.classList.toggle("active");
+
+class Menu {
+    constructor(menuToOpen) {
+      this.menuToOpen = menuToOpen
+    }
+  
+    openMenu() {
+      this.menuToOpen.classList.toggle('active')
+    }
+  }
+  
+  var menu = document.getElementById('menu')
+  function animacaoDoMenu() {
+    new Menu(menu).openMenu()
+  }
+
+  
+class Social {
+    constructor(list, drop, seta) {
+        this.list = list
+        this.drop = drop
+        this.seta = seta
+    }
+
+    setSocial() {
+    this.seta.classList.toggle("active");
 
     var isMobile = window.matchMedia("(max-width: 720px)").matches; 
 
-    if (list.style.display === "none" || list.style.display === "") {
-        list.style.display = "block"
+    if (this.list.style.display === "none" || this.list.style.display === "") {
+        this.list.style.display = "block"
         if (isMobile) {
-            drop.style.marginTop = "255px"
+            this.drop.style.marginTop = "255px"
         } else {
-            drop.style.marginTop = "566px"
+            this.drop.style.marginTop = "566px"
         }
     } else {
-        list.style.display = "none"
+        this.list.style.display = "none"
         if (isMobile) {
-            drop.style.marginTop = "255px"
+            this.drop.style.marginTop = "255px"
         } else {
-            drop.style.marginTop = "370px"
+            this.drop.style.marginTop = "370px"
         }
     }
+        
+    }
+}
+
+    var social = document.getElementById("social");
+    var dropdown = document.getElementById("dropdown");
+    var setaAnimada = document.getElementById("setaAnimada");
+
+function mostrarRedesSociais() {
+    new Social(social, dropdown, setaAnimada).setSocial()
 }
 
 
